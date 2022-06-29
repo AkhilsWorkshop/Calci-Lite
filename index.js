c2d36eafaeb6e8715599c6402136711dc78e3f65
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-06-28 23:06:47 -0500

    Added Visitor Count

---
+++ index.js
@@ -0,0 +1,19 @@
+  function counts(response) {
+    document.getElementById('count').innerText = response.value;
+  }
+
+  function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+  }
